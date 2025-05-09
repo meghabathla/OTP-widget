2e5e4f474147a83efceaa6ab0629017cf7d4c245
Megha <bathlamegha6@gmail.com>
2025-05-10 00:25:09 +0530

    done

--- src/components/Otpfield.tsx
+++ src/components/Otpfield.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export const Otpfield = () => {
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+  const handleNextFocus = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const value = e.target.value;
+    if (index < inputRefs.current.length - 1 && value.length === 1) {
+      inputRefs.current[index + 1]?.focus();
+    }
+  };
   return (
     <div>
       {[...Array(4)].map((_, index) => (
@@ -10,6 +20,8 @@ export const Otpfield = () => {
           className="input-field"
           maxLength={1}
           pattern="[0-9]"
+          onChange={(e) => handleNextFocus(e, index)}
+          ref={(el) => (inputRefs.current[index] = el)}
         />
       ))}
     </div>
