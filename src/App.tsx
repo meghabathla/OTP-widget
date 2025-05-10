56b0c301f3421e18a9f2aad6a35beffd0c47e3ff
Megha <bathlamegha6@gmail.com>
2025-05-10 20:18:47 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import { Otpfield } from "./components/Otpfield";
 function App() {
   return (
     <>
+      <div>Enter the verification code sent on info@gmail.com</div>
       <Otpfield />
+      <div>Didn't recieve the code?</div>
+      <button>Resend</button>
     </>
   );
 }
--- src/components/Otpfield.tsx
+++ src/components/Otpfield.tsx
@@ -1,19 +1,30 @@
-import React, { useRef } from "react";
+import React, { useMemo, useRef } from "react";
 
 export const Otpfield = () => {
-  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+  const arr = useMemo(() => [...Array(4)], []);
+  const inputRefs = useRef<HTMLInputElement[]>([]);
   const handleNextFocus = (
     e: React.ChangeEvent<HTMLInputElement>,
     index: number
   ) => {
+    console.log("onchnage  ", e.target.value);
     const value = e.target.value;
     if (index < inputRefs.current.length - 1 && value.length === 1) {
       inputRefs.current[index + 1]?.focus();
     }
   };
+  const handleBackFocus = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    console.log("onkeydown ", e.key);
+    if (e.key === "Backspace" && inputRefs.current[index]?.value.length === 0) {
+      inputRefs.current[index - 1]?.focus();
+    }
+  };
   return (
     <div>
-      {[...Array(4)].map((_, index) => (
+      {arr.map((_, index) => (
         <input
           key={index}
           type="text"
@@ -21,7 +32,8 @@ export const Otpfield = () => {
           maxLength={1}
           pattern="[0-9]"
           onChange={(e) => handleNextFocus(e, index)}
-          ref={(el) => (inputRefs.current[index] = el)}
+          onKeyDown={(e) => handleBackFocus(e, index)}
+          ref={(el: HTMLInputElement) => (inputRefs.current[index] = el)}
         />
       ))}
     </div>
